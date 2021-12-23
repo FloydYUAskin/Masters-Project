c48c2ce4a7b191ea763b8af65ec3eda5b2141742
Floyd Askin <FloydYUAskin@gmail.com.com>
2021-12-23 00:05:01 +0000

    Tidying up workspace and merging function fitting branch onto master

--- function_fitting.py
+++ function_fitting.py
@@ -1,106 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from wave_functions import *
-import warnings
 
-def closestVariableEnergy(function, variableSet, nSet, accuracy, target):
-    xVariableSet = np.linspace(variableSet[0][0], variableSet[0][1], accuracy)
-    yVariableSet = np.linspace(variableSet[1][0], variableSet[1][1], accuracy)
-    zVariableSet = np.linspace(variableSet[2][0], variableSet[2][1], accuracy)
+boxVarMin, boxVarMax = 0.4, 0.7
+oscVarMin, oscVarMax = 1, 4
+accuracy = 50
 
-    variableMesh = np.array(np.meshgrid(xVariableSet, yVariableSet, zVariableSet))
-    resultsMesh = np.ones_like(variableMesh[0])
+targetValues = np.zeros((3,3,3))
+targetValues[1,0,0] = 73
+targetValues[0,1,0] = 80.2
+targetValues[0,0,1] = 101.1
+targetValues[2,0,0] = 159.3
+targetValues[1,1,0] = 160.2
+targetValues[1,0,1] = 173.3
+targetValues[0,2,0] = 181.9
+targetValues[0,1,1] = 184.1
+targetValues[0,0,2] = 219.4
 
-    for xIndex, xValue in enumerate(xVariableSet):
-        for yIndex, yValue in enumerate(yVariableSet):
-            for zIndex, zValue in enumerate(zVariableSet):
-                testValues = function([xValue, yValue, zValue], nSet, h2Mass)
-                difference = np.abs(testValues - target)
-                totalDifference = np.nansum(difference)
+transitionList = [[Index, Value] for Index, Value in np.ndenumerate(targetValues) if Value != 0]
 
-                resultsMesh[xIndex, yIndex, zIndex] = totalDifference
+boxVariableArray = np.linspace(boxVarMin, boxVarMax, accuracy)
+oscVariableArray = np.linspace(oscVarMin, oscVarMax, accuracy)
 
+differenceListBox = []
+variableListBox = []
 
-    minimumIndex = np.where(resultsMesh == np.nanmin(resultsMesh))
+for xIndex, xVariable in enumerate(boxVariableArray):
+    for yIndex, yVariable in enumerate(boxVariableArray):
+        for zIndex, zVariable in enumerate(boxVariableArray):
+            currentStep = (xIndex * accuracy ** 2) + (yIndex * accuracy) + zIndex
+            print(f'{currentStep * 100 / accuracy**3}%')
+            difference = 0
+            for [transition, value] in transitionList:
+                transitionBox = particleInABox3DEnergy([xVariable, yVariable, zVariable], [transition[0]+1, transition[1]+1, transition[2]+1], h2Mass) - particleInABox3DEnergy([xVariable, yVariable, zVariable], [1,1,1], h2Mass)
+                difference += np.abs(transitionBox - value)
+            variableListBox.append([xVariable, yVariable, zVariable])
+            differenceListBox.append(difference)
+            os.system('clear')
 
+differenceListBox = np.array(differenceListBox)
+variableListBox = np.array(variableListBox)
 
-    xClosestVariable = variableMesh[0][minimumIndex]
-    yClosestVariable = variableMesh[1][minimumIndex]
-    zClosestVariable = variableMesh[2][minimumIndex]
+differencesInd = np.argsort(differenceListBox)
 
-    return resultsMesh, [xClosestVariable, yClosestVariable, zClosestVariable], minimumIndex, [xVariableSet, yVariableSet, zVariableSet]
+orderedDifferenceBox = differenceListBox[differencesInd]
+orderedVariablesBox = variableListBox[differencesInd]
 
+differenceList = []
+variableList = []
 
-def variableSearchEnergy(function, variableSet, nSet, accuracy, depth, target):
-    resultsMesh, variableList, minimumIndex, variableSets = closestVariableEnergy(function, variableSet, nSet, accuracy, target)
-    print(minimumIndex)
-    for iteration in range(depth):
-        xMinimumIndex = minimumIndex[0][0] - 1
-        xMaximumIndex = minimumIndex[0][0] + 1
-        yMinimumIndex = minimumIndex[1][0] - 1
-        yMaximumIndex = minimumIndex[1][0] + 1
-        zMinimumIndex = minimumIndex[2][0] - 1
-        zMaximumIndex = minimumIndex[2][0] + 1
-
-        if xMaximumIndex >= len(variableSets[0]):
-            xMaximumIndex -= 1
-        if yMaximumIndex >= len(variableSets[1]):
-            yMaximumIndex -= 1
-        if zMaximumIndex >= len(variableSets[2]):
-            zMaximumIndex -= 1
-
-        xMinimumVariable = variableSets[0][xMinimumIndex]
-        xMaximumVariable = variableSets[0][xMaximumIndex]
-        yMinimumVariable = variableSets[1][yMinimumIndex]
-        yMaximumVariable = variableSets[1][yMaximumIndex]
-        zMinimumVariable = variableSets[2][zMinimumIndex]
-        zMaximumVariable = variableSets[2][zMaximumIndex]
-
-        variableSet = [xMinimumVariable, xMaximumVariable], [yMinimumVariable, yMaximumVariable], [zMinimumVariable, zMaximumVariable]
-
-        resultsMesh, variableList, minimumIndex, variableSets = closestVariableEnergy(function, variableSet, nSet, accuracy, target)
-
-
-
-    return variableList
+for xIndex, xVariable in enumerate(oscVariableArray):
+    for yIndex, yVariable in enumerate(oscVariableArray):
+        for zIndex, zVariable in enumerate(oscVariableArray):
+            currentStep = (xIndex * accuracy ** 2) + (yIndex * accuracy) + zIndex
+            print(f'{currentStep * 100 / accuracy**3}%')
+            difference = 0
+            for [transition, value] in transitionList:
+                transitionBox = simpleHarmonicOscillator3DEnergy([xVariable, yVariable, zVariable], [transition[0], transition[1], transition[2]], h2Mass) - simpleHarmonicOscillator3DEnergy([xVariable, yVariable, zVariable], [0,0,0], h2Mass)
+                difference += np.abs(transitionBox - value)
+            variableList.append([xVariable, yVariable, zVariable])
+            differenceList.append(difference)
+            os.system('clear')
 
 
+differenceList = np.array(differenceList)
+variableList = np.array(variableList)
 
-if __name__ == '__main__':
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        nRange = np.arange(0,3,1)
-        nSetOscillator = np.array(np.meshgrid(nRange,nRange,nRange)).astype('float')
-        nRange = np.arange(1,4,1)
-        nSetBox = np.array(np.meshgrid(nRange,nRange,nRange)).astype('float')
+differencesInd = np.argsort(differenceList)
 
-        targetValues = np.zeros((3,3,3))
-        targetValues[1,0,0] = 73
-        targetValues[0,1,0] = 80.2
-        targetValues[0,0,1] = 101.1
-        targetValues[2,0,0] = 159.3
-        targetValues[1,1,0] = 160.2
-        targetValues[1,0,1] = 173.3
-        targetValues[0,2,0] = 181.9
-        targetValues[0,1,1] = 184.1
-        targetValues[0,0,2] = 219.4
+orderedDifference = differenceList[differencesInd]
+orderedVariables = variableList[differencesInd]
 
-        targetValues[targetValues==0] = np.nan
-        nSetOscillator[targetValues==0] = np.nan
-        nSetBox[targetValues==0] = np.nan
+closestOsc = [simpleHarmonicOscillator3DEnergy(orderedVariables[0], [n,0,0], h2Mass) - simpleHarmonicOscillator3DEnergy(orderedVariables[0], [0,0,0], h2Mass) for n in range(0,3)]
+print(f'Deviance in Simple  Harmonic OScillator: {closestOsc - targetValues[:,0,0]}')
+print(f'With Spring constants of: {orderedVariables[0]}')
+closestBox = [particleInABox3DEnergy(orderedVariablesBox[0], [n,1,1],h2Mass) - particleInABox3DEnergy(orderedVariablesBox[0], [1,1,1],h2Mass) for n in range(1,4)]
+print(f'Deviance on Particle in a Box: {closestBox - targetValues[:,0,0]}')
+print(f'Box Size: {orderedVariablesBox[0]}')
 
 
-
-        closestVariablesParticleInABox = variableSearchEnergy(particleInABox3DEnergy, [[0,10],[0,10],[0,10]], nSet, 10, 10, targetValues)
-        closestVariablesSimpleHarmonicOscillator = variableSearchEnergy(simpleHarmonicOscillator3DEnergy, [[0,10],[0,10],[0,10]], nSet, 10, 10, targetValues)
-        print(closestVariablesParticleInABox, closestVariablesSimpleHarmonicOscillator)
-
-        approximateBox = particleInABox3DEnergy(closestVariablesParticleInABox, nSet, h2Mass)
-        approximateOscillator = simpleHarmonicOscillator3DEnergy(closestVariablesSimpleHarmonicOscillator, nSet, h2Mass)
-
-        plt.hlines(approximateBox[:,0,0],0,1)
-        plt.hlines(approximateOscillator[:,0,0],1,2)
-        plt.hlines(targetValues[:,0,0],0,2,color='r')
-        plt.show()
-
-        print(targetValues[:,0,0], approximateOscillator[:,0,0])
+plt.hlines(closestBox, 0, 1)
+plt.hlines(closestOsc, 1, 2)
+plt.hlines(targetValues[:,0,0], 0, 2, color='r')
+plt.show()
